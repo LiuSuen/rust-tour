3487335df6373907b2a3870e124423bcf8199594
LiuSuen <84234596+LiuSuen@users.noreply.github.com>
2023-01-25 13:51:30 -0500

    Initial commit

---
+++ Project1/src/main.rs
@@ -0,0 +1,32 @@
+//A command-line tool to solve the Chicken-and-Rabbit problem.
+use std::io;
+
+fn main() {
+    println!("Please enter the number of heads:");
+    let mut heads = String::new();
+    io::stdin()
+        .read_line(&mut heads)
+        .expect("Failed to read input");
+    let heads = heads.trim().parse().expect("Please enter a valid number");
+
+    println!("Please enter the number of legs:");
+    let mut legs = String::new();
+    io::stdin()
+        .read_line(&mut legs)
+        .expect("Failed to read input");
+    let legs = legs.trim().parse().expect("Please enter a valid number");
+
+    x = (heads*4-legs)/2
+    if heads!=0 && (heads*4-legs)%(r*2) == 0 {
+      y = heads-x
+      if x<0 || y<0 {
+        printIn!("There is no integer solution for provided number: {} heads and {} legs.", heads, legs)
+      }
+      else{
+        printIn!("There are {} rabbits and {} chicken in the yard.", x,y)
+      }
+    }
+    else{
+      printIn!("There is no integer solution for provided number: {} heads and {} legs.", heads, legs)
+    }
+}
